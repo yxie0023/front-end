b91a0c8763aa63cb5a347353a629f4e30d68509d
DELL <1234>
2021-04-02 19:40:08 +0800

    ip changes

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@
 import request from "@/api/request";
 
 // 登录接口  http://www.superprotector.tk:8880
-export const getAll = data => request.post("http://172.16.173.109:8880/vacTenToNineteen/getAll", data);
-export const getVaccineList = data => request.post("http://172.16.173.109:8880/combinedVaccineDescEffects/getAllList", data);
+export const getAll = data => request.post("http://18.217.25.170:8880/vacTenToNineteen/getAll", data);
+export const getVaccineList = data => request.post("http://18.217.25.170:8880/combinedVaccineDescEffects/getAllList", data);
 
